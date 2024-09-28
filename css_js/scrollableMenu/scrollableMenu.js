a4ea14b6ce16e00bdbc3946d6d9e99f40227b452
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-09-28 20:24:53 +0500

    add scrollable menu but not connection

---
+++ css_js/scrollableMenu/scrollableMenu.js
@@ -0,0 +1,74 @@
+const tabs = document.querySelectorAll(".scrollable-tabs-container a");
+const rightArrow = document.querySelector(
+  ".scrollable-tabs-container .right-arrow i"
+);
+const leftArrow = document.querySelector(
+  ".scrollable-tabs-container .left-arrow i"
+);
+const tabsList = document.querySelector(".scrollable-tabs-container ul");
+const leftArrowContainer = document.querySelector(
+  ".scrollable-tabs-container .left-arrow"
+);
+const rightArrowContainer = document.querySelector(
+  ".scrollable-tabs-container .right-arrow"
+);
+
+const removeAllActiveClasses = () => {
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+};
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    removeAllActiveClasses();
+    tab.classList.add("active");
+  });
+});
+
+const manageIcons = () => {
+  if (tabsList.scrollLeft >= 20) {
+    leftArrowContainer.classList.add("active");
+  } else {
+    leftArrowContainer.classList.remove("active");
+  }
+  let maxScrollValue = tabsList.scrollWidth - tabsList.clientWidth - 20;
+  console.log("scrollWindth: ", tabsList.scrollWidth);
+  console.log("clientWindth: ", tabsList.clientWidth);
+  if (tabsList.scrollLeft >= maxScrollValue) {
+    rightArrowContainer.classList.remove("active");
+  } else {
+    rightArrowContainer.classList.add("active");
+  }
+};
+
+rightArrow.addEventListener("click", () => {
+  tabsList.scrollLeft += 200;
+  manageIcons();
+});
+
+leftArrow.addEventListener("click", () => {
+  tabsList.scrollLeft -= 200;
+  manageIcons();
+});
+
+tabsList.addEventListener("scroll", manageIcons);
+
+let dragging = false;
+
+const drag = (e) => {
+    if(!dragging) return;
+    tabsList.classList.add("dragging");
+    tabsList.scrollLeft -= e.movementX;
+}
+
+tabsList.addEventListener("mousedown", () => {
+  dragging = true;
+});
+
+tabsList.addEventListener("mousemove", drag);
+
+document.addEventListener("mouseup", () => {
+    dragging = false;
+    tabsList.classList.remove("dragging")
+})
